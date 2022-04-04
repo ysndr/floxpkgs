24c345de26eb3d4ccf90b443833c5b05ead74ac5
EC2 Default User <ec2-user@ip-172-31-23-154.eu-west-2.compute.internal>
2022-04-04 15:46:59 +0000

    init hello-python-library

---
+++ pythonPackages/hello-python-library/default.nix
@@ -0,0 +1,21 @@
+{ buildPythonPackage
+}:
+
+
+buildPythonPackage {
+  project = "hello-python-library";
+
+  # Programs and libraries used only at build-time.
+  nativeBuildInputs = [ ];
+
+  # Programs and libraries used by the new derivation at run-time.
+  buildInputs = [ ];
+
+  # The propagated equivalent of buildInputs. Note that python modules
+  # must always be listed in this section on account of the way that the
+  # python module support works in Nix.
+  propagatedBuildInputs = [ ];
+
+  # See https://nixos.org/manual/nixpkgs/stable/#ssec-stdenv-dependencies for
+  # more information on the various ways of specifying dependencies with Nix.
+}
